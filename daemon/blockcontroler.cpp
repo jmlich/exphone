0f6d05c99f691fb10d5224cf31d79bc917e3671b
Jozef Mlich <jmlich83@gmail.com>
2024-12-06 18:38:19 +0100

    Update history even if not blocked

--- daemon/blockcontroler.cpp
+++ daemon/blockcontroler.cpp
@@ -28,8 +28,8 @@ void BlockControler::incomingCall(const QString &_callerId, const QString &calle
     QString callerId = _callerId;
     bool blocked = m_blockModel.isBlocked(callerId);
     qDebug() << Q_FUNC_INFO << callerId << callerName << blocked;
+    m_blockModel.upsertItem(callerId, callerName);
     if (blocked) {
-        m_blockModel.upsertItem(callerId, callerName);
 #if defined(MER_EDITION_SAILFISH) || defined(UUITK_EDITION)
         m_voiceCallController.hangup();
 #endif
