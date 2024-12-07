251b1237c0b5c67d840cf5d247ffc973771b621e
Jozef Mlich <jmlich83@gmail.com>
2024-12-07 14:35:38 +0100

    translations

---
+++ ui/translations/exphone.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DownloadDataMenuItem</name>
+    <message>
+        <location filename="../qml/components/DownloadDataMenuItem.qml" line="6"/>
+        <source>Download Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <source>Add number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <source>Update details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="27"/>
+        <location filename="../qml/pages/EditPage.qml" line="27"/>
+        <source>Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="33"/>
+        <location filename="../qml/pages/EditPage.qml" line="33"/>
+        <source>Contact name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="37"/>
+        <location filename="../qml/pages/EditPage.qml" line="37"/>
+        <source>Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="42"/>
+        <location filename="../qml/pages/EditPage.qml" line="42"/>
+        <source>Count: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="47"/>
+        <location filename="../qml/pages/EditPage.qml" line="47"/>
+        <source>Last seen: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="53"/>
+        <location filename="../qml/pages/EditPage.qml" line="53"/>
+        <source>Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="63"/>
+        <location filename="../qml/pages/EditPage.qml" line="63"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="75"/>
+        <location filename="../qml/pages/EditPage.qml" line="75"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GraphData</name>
+    <message>
+        <location filename="../qml/components/GraphData.qml" line="304"/>
+        <source>No data</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ListPage</name>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="7"/>
+        <location filename="../qml/pages/ListPage.qml" line="7"/>
+        <source>Blocked numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="11"/>
+        <location filename="../qml/pages/ListPage.qml" line="11"/>
+        <source>Add number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="21"/>
+        <location filename="../qml/pages/ListPage.qml" line="21"/>
+        <source>No records, make phone call or add number manually</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="52"/>
+        <location filename="../qml/pages/ListPage.qml" line="52"/>
+        <source>Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="52"/>
+        <location filename="../qml/pages/ListPage.qml" line="52"/>
+        <source>Not blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
