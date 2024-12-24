3ab52c71bd59a0ecfdbda790274f48cba8fe2408
Jozef Mlich <jozef.mlich@greycortex.com>
2024-12-24 08:20:38 +0100

    lupdate

--- ui/translations/exphone.ts
+++ ui/translations/exphone.ts
@@ -1,62 +1,54 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="en">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
 <context>
     <name>EditPage</name>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="8"/>
         <location filename="../qml/pages/EditPage.qml" line="8"/>
         <source>Update details</source>
         <translation>Update details</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="8"/>
-        <location filename="../qml/pages/EditPage.qml" line="8"/>
         <source>Add phone number</source>
         <translation>Add phone number</translation>
     </message>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="26"/>
         <location filename="../qml/pages/EditPage.qml" line="26"/>
         <source>Phone Number</source>
         <translation>Phone Number</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="30"/>
-        <location filename="../qml/pages/EditPage.qml" line="30"/>
         <source>+420123456789</source>
         <translation>+420123456789</translation>
     </message>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="36"/>
         <location filename="../qml/pages/EditPage.qml" line="36"/>
         <source>Contact Name</source>
         <translation>Contact Name</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="40"/>
-        <location filename="../qml/pages/EditPage.qml" line="40"/>
         <source>John Doe</source>
         <translation>John Doe</translation>
     </message>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="44"/>
         <location filename="../qml/pages/EditPage.qml" line="44"/>
         <source>Reason for Blocking</source>
         <translation>Reason for Blocking</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="49"/>
-        <location filename="../qml/pages/EditPage.qml" line="49"/>
         <source>Spam, Phishing, Telemarketing, Abuse, ...</source>
         <translation>Spam, Phishing, Telemarketing, Abuse, ...</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="54"/>
-        <location filename="../qml/pages/EditPage.qml" line="54"/>
         <source>Calls: %1</source>
         <translation>Calls: %1</translation>
     </message>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="59"/>
         <location filename="../qml/pages/EditPage.qml" line="59"/>
         <source>Last Call Date:
 %1</source>
@@ -65,18 +57,15 @@
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="67"/>
-        <location filename="../qml/pages/EditPage.qml" line="67"/>
         <source>Blocking</source>
         <translation>Blocking</translation>
     </message>
     <message>
         <location filename="../qml/pages/EditPage.qml" line="77"/>
-        <location filename="../qml/pages/EditPage.qml" line="77"/>
         <source>Save</source>
         <translation>Save</translation>
     </message>
     <message>
-        <location filename="../qml/pages/EditPage.qml" line="89"/>
         <location filename="../qml/pages/EditPage.qml" line="89"/>
         <source>Delete</source>
         <translation>Delete</translation>
@@ -86,33 +75,51 @@
     <name>ListPage</name>
     <message>
         <location filename="../qml/pages/ListPage.qml" line="7"/>
-        <location filename="../qml/pages/ListPage.qml" line="7"/>
         <source>Blocked numbers</source>
         <translation>Blocked numbers</translation>
     </message>
     <message>
         <location filename="../qml/pages/ListPage.qml" line="11"/>
-        <location filename="../qml/pages/ListPage.qml" line="11"/>
         <source>Add number</source>
         <translation>Add number</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ListPage.qml" line="21"/>
-        <location filename="../qml/pages/ListPage.qml" line="21"/>
+        <location filename="../qml/pages/ListPage.qml" line="29"/>
         <source>No records, make phone call or add number manually</source>
         <translation>No records, make phone call or add number manually</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ListPage.qml" line="56"/>
-        <location filename="../qml/pages/ListPage.qml" line="56"/>
+        <location filename="../qml/pages/ListPage.qml" line="64"/>
         <source>Blocked</source>
         <translation>Blocked</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ListPage.qml" line="56"/>
-        <location filename="../qml/pages/ListPage.qml" line="56"/>
+        <location filename="../qml/pages/ListPage.qml" line="64"/>
         <source>Not blocked</source>
         <translation>Not blocked</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="8"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="19"/>
+        <source>Block unknown numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="24"/>
+        <source>Block anonymous numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="29"/>
+        <source>Save</source>
+        <translation type="unfinished">Save</translation>
     </message>
 </context>
 </TS>
