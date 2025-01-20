78ad5fcfd96eb5f3e6d3e252efb04402c7940bdb
buchnerh <33247799+buchnerh@users.noreply.github.com>
2025-01-20 20:27:17 +0100

    Create exphone_de.ts
    German translation

---
+++ ui/translations/exphone_de.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="de">
+<context>
+    <name>EditPage</name>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <source>Update details</source>
+        <translation>Details bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="8"/>
+        <source>Add phone number</source>
+        <translation>Telefonnummer hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="26"/>
+        <source>Phone Number</source>
+        <translation>Telefonnummer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="30"/>
+        <source>+420123456789</source>
+        <translation>+420123456789</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="36"/>
+        <source>Contact Name</source>
+        <translation>Kontaktname</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="40"/>
+        <source>John Doe</source>
+        <translation>John Doe</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="44"/>
+        <source>Reason for Blocking</source>
+        <translation>Grund für Blockade</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="49"/>
+        <source>Spam, Phishing, Telemarketing, Abuse, ...</source>
+        <translation>Spam, Phishing, Telemarketing, Missbrauch, ...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="54"/>
+        <source>Calls: %1</source>
+        <translation>Anrufe: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="59"/>
+        <source>Last Call Date: %1</source>
+        <translation>Letzter Anruf: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="67"/>
+        <source>Blocking</source>
+        <translation>Blockieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="77"/>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditPage.qml" line="89"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
+<context>
+    <name>ListPage</name>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="7"/>
+        <source>Blocked numbers</source>
+        <translation>Blockierte Nummern</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="11"/>
+        <source>Add number</source>
+        <translation>Nummer hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="19"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="29"/>
+        <source>No records, make phone call or add number manually</source>
+        <translation>Keine Aufzeichnungen, anrufen oder Nummer manuell hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="53"/>
+        <source>Anonymous caller</source>
+        <translation>Anonymer Anrufer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="71"/>
+        <source>Blocked</source>
+        <translation>Blockiert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="71"/>
+        <source>Not blocked</source>
+        <translation>Nicht blockiert</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="8"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="19"/>
+        <source>Block unknown numbers (Contacts)</source>
+        <translation>Unbekannte Nummern blockieren (Kontakte)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="24"/>
+        <source>Block anonymous numbers (CLIR)</source>
+        <translation>Anonyme Nummern blockieren (CLIR)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="28"/>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+</context>
+</TS>
